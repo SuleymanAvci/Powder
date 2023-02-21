afedeb709eda6349b378815a600d405b45ac7df8
sa1avci <sa1avci@hotmail.com>
2023-02-22 00:43:32 +0300

    Category işlemleri eklendi

--- Powder/Areas/Admin/Controllers/CategoryController.cs
+++ Powder/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.ActionConstraints;
 using Powder.Entities;
 using Powder.Interfaces;
 using Powder.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Powder.Areas.Admin.Controllers
 {
@@ -25,7 +26,7 @@ namespace Powder.Areas.Admin.Controllers
             return View(new CategoryAddModel());
         }
         [HttpPost]
-        public IActionResult Add(CategoryAddModel model) 
+        public IActionResult Add(CategoryAddModel model)
         {
             if (ModelState.IsValid)
             {
@@ -36,6 +37,39 @@ namespace Powder.Areas.Admin.Controllers
                 return RedirectToAction("Index");
             }
             return View(model);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var getCategory = _categoryRepository.Get(id);
+            CategoryUpdateModel model = new CategoryUpdateModel()
+            {
+                Id = getCategory.Id,
+                Name = getCategory.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(CategoryUpdateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var updatedCategory = _categoryRepository.Get(model.Id);
+                if (updatedCategory != null)
+                {
+                    updatedCategory.Name = model.Name;
+                    _categoryRepository.Update(updatedCategory);
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _categoryRepository.Delete(new Category {Id=id });
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Powder/Models/CategoryUpdateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Powder.Models
+{
+    public class CategoryUpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı boş geçilemz.")]
+        public string Name { get; set; }
+    }
+}
